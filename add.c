d1b846f88347dfe4009beb8ea18f9617b66c67ee
Ashu-1309 <60690692+Ashu-1309@users.noreply.github.com>
2020-10-03 11:23:23 +0530

    Update add.c
    Adding comment helps reader to understand the code

--- add.c
+++ add.c
@@ -3,7 +3,7 @@ int main()  //main function
 {
   double a,b;
   printf("Enter two numbers:");
-  scanf("%ld%ld",&a,&b);
-  printf("Addition of two numbers=%ld",a+b);
+  scanf("%ld%ld",&a,&b); //It will two number as input
+  printf("Addition of two numbers=%ld",a+b); // Display added number
   return 0;
 }
